eebb5af3c34c0591a534a1be7244f73c22c9a5cc
facumoron <facumartin09@hotmail.com>
2022-04-27 15:08:59 -0300

    commit

--- primer entrega/index.js
+++ primer entrega/index.js
@@ -2,7 +2,7 @@ let usuario = prompt("Ingrese su usuario");
 while (usuario == "") {
     if (usuario == "") {
         usuario = prompt("Porfavor ingrese un usuario valido");
-    }else if (usuario != "") {
+    } else if (usuario != "") {
         alert("Hola " + usuario)
         break;
     }
@@ -14,7 +14,7 @@ let contrasenia = prompt("Ahora ingrese su contrasenia");
 while (contrasenia == "") {
     if (contrasenia == "") {
         contrasenia = prompt("Porfavor ingrese una contrasenia valida");
-    }else if (contrasenia != "") {
+    } else if (contrasenia != "") {
         break;
     }
 }
@@ -28,10 +28,10 @@ while (login != "") {
             while (nuevoUsuario == "") {
                 if (nuevoUsuario == "") {
                     nuevoUsuario = prompt("Porfavor ingrese un usuario valido");
-                }else if (nuevoUsuario != "") {
+                } else if (nuevoUsuario != "") {
                     break;
                 }
-            }ña
+            } ña
             alert(`Su usuario fue cambiado exitosamente a ${nuevoUsuario}`);
             break;
         case "2":
@@ -39,7 +39,7 @@ while (login != "") {
             while (nuevaContrasenia == "") {
                 if (nuevaContrasenia == "") {
                     nuevaContrasenia = prompt("Porfavor ingrese un contrasenia valido");
-                }else if (nuevaContrasenia != "") {
+                } else if (nuevaContrasenia != "") {
                     break;
                 }
             }
@@ -63,19 +63,45 @@ const producto6 = `Pipeta para 41 a 60 kilos`;
 let peso = prompt("Por favor escriba cuanto pesa su mascota en kilos para que el sistema calcule que tipo de pipeta necesita");
 if (peso >= 2, peso <= 5) {
     alert("usted tendria que comprar el producto 1");
-} else if (peso >= 6, peso <=10) {
-    alert("usted tendria que comprar el producto 2");  
-} else if (peso >= 11, peso <=20) {
-    alert("usted tendria que comprar el producto 3");  
-} else if (peso >= 21, peso <=30) {
-    alert("usted tendria que comprar el producto 4");  
-} else if (peso >= 31, peso <=40) {
-    alert("usted tendria que comprar el producto 5");  
-} else if (peso >= 41, peso <=60) {
-    alert("usted tendria que comprar el producto 6");  
+} else if (peso >= 6, peso <= 10) {
+    alert("usted tendria que comprar el producto 2");
+} else if (peso >= 11, peso <= 20) {
+    alert("usted tendria que comprar el producto 3");
+} else if (peso >= 21, peso <= 30) {
+    alert("usted tendria que comprar el producto 4");
+} else if (peso >= 31, peso <= 40) {
+    alert("usted tendria que comprar el producto 5");
+} else if (peso >= 41, peso <= 60) {
+    alert("usted tendria que comprar el producto 6");
 }
 
-let menu = prompt("Escriba el numero de producto que quiere comprar")
 
+let menu = prompt("Escriba el numero de producto que quiere comprar")
+while (menu != "") {
+    switch (menu) {
+        case 1:
+            alert("el producto cuesta 300 pesos")
+            break;
+        case 2:
+            alert("el producto cuesta 500 pesos")
+            break;
+        case 3:
+            alert("el producto cuesta 600 pesos")
+            break;
+        case 4:
+            alert("el producto cuesta 700 pesos")
+            break;
+        case 5:
+            alert("el producto cuesta 900 pesos")
+            break;
+        case 6:
+            alert("el producto cuesta 1200 pesos")
+            break;
+
+        default:
+            break;
+
+    }
+}
 
 
